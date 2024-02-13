cd8fe2279535af8b1c61c93a2f3161c919866665
Stiven Diaz <steven_el22@hotmail.com>
2024-02-12 20:34:26 -0800

    modifit js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-  const input = document.querySelector('text_List');
-  const button =document.querySelector('button');
+const input = document.querySelector('#text_List');
+  const button = document.querySelector('button');
+
   const listUsers = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await response.json();
 
+  function callUser(id) {
+
+    let user = users.find((user) => user.id == id)
+    let userTableBody = ``
+
+    userTableBody = `<tr>
+   <td class='centered'>${user.id}</td>
+   <td class='centered'>${user.name}</td>
+   <td class='centered'>${user.phone}</td>
+   </tr>`;
+
+   document.getElementById("tableBody_FoundUser").innerHTML = userTableBody;
+  }
+
   button.addEventListener('click', (e) => {
     e.preventDefault();
-    tableBody(input.value);
+
+    callUser(input.value);
   });
  
   let tableBody = ``;
@@ -24,4 +40,3 @@
 window.addEventListener("load", function () {
   listUsers();
 });
-
